1d8e81f9db0b1191550d15dffd3d89b6741aba0c
William Sterling <william.sterling@pinnacol.com>
2019-04-21 21:40:06 -0600

    install civic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import img from './logo.svg';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={img} className="App-logo" alt="logo" /> */}
+
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This is Raining.design
           </p>
-          <a
+          {/* <p>
+            <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            rel="noopener noreferrer">
+              You can login here.
+            </a>
+          </p> */}
+
+          <button id="signupButton" className="civic-button-a medium" type="button">
+              <span>Log in with Civic</span>
+          </button>
         </header>
       </div>
     );
